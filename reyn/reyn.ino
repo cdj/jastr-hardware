24b7fb8f2ebb005f4810d27095da27aedda5a368
Carl Jamilkowski <carl@jamilkowski.com>
2015-06-15 23:39:15 -0400

    Update pins

--- reyn/reyn.ino
+++ reyn/reyn.ino
@@ -9,7 +9,7 @@
 // Connect CLK/MISO/MOSI to hardware SPI
 // e.g. On UNO & compatible: CLK = 13, MISO = 12, MOSI = 11
 #define ADAFRUITBLE_REQ 10
-#define ADAFRUITBLE_RDY 8     // This should be an interrupt pin, on Uno thats #2 or #3
+#define ADAFRUITBLE_RDY 2     // This should be an interrupt pin, on Uno thats #2 or #3
 #define ADAFRUITBLE_RST 9
 
 Adafruit_BLE_UART BTLEserial = Adafruit_BLE_UART(ADAFRUITBLE_REQ, ADAFRUITBLE_RDY, ADAFRUITBLE_RST);
